da5e115913a41e06b50af1416fc973e4e56f238e
Ahnaf Rayhan Nurducha <ahnafrayn21@gmail.com>
2026-09-23 17:55:49 +0700

    Labwork 01 - Unguided

--- src/lw01/unguided/Main.java
+++ src/lw01/unguided/Main.java
@@ -1,35 +1,48 @@
 package lw01.unguided;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        
-        List<WashService> washes = new ArrayList<>();
+        try {
+            Scanner scanner = new Scanner(new File("src/lw01/unguided/washes.txt"));
+            if (!scanner.hasNextInt()) {
+                scanner.close();
+                return;
+            }
+            
+            int totalRecords = scanner.nextInt();
+            WashService[] services = new WashService[totalRecords];
+            int[] unitsArray = new int[totalRecords]; 
 
-        try (Scanner scanner = new Scanner(Main.class.getResourceAsStream("washes.txt"))) {
-    
-            while (scanner.hasNext()) {
+            for (int i = 0; i < totalRecords; i++) {
                 String type = scanner.next();
                 String id = scanner.next();
                 int days = scanner.nextInt();
                 int units = scanner.nextInt();
+                
+                unitsArray[i] = units;
 
-                if (type.equals("Car")) {
-                    washes.add(new CarWash(id, days));
-                } else if (type.equals("Motorcycle")) {
-                    washes.add(new MotorcycleWash(id, days));
+                if (type.equals("MOTORCYCLE")) {
+                    services[i] = new MotorcycleWash(id, days);
+                } else if (type.equals("CAR")) {
+                    services[i] = new CarWash(id, days);
                 }
             }
 
-            for (WashService wash : washes) {
-                System.out.println(wash.summary());
+            for (int i = 0; i < totalRecords; i++) {
+                String output = services[i].getId() + " | " 
+                              + services[i].label() + " | " 
+                              + services[i].calculateCharge(unitsArray[i]);
+                System.out.println(output);
             }
-
-        } 
-        
+            
+            scanner.close();
+            
+        } catch (FileNotFoundException e) {
+            System.err.println("File washes.txt tidak ditemukan.");
+        }
     }
 }
